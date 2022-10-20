91fe128838e775e39a0b5b13f6258ec206b3cbf3
wyclife12dube <wyclife12dube@gmail.com>
2022-10-20 02:56:19 -0700

    Prints formated string

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - prints formated string
+ * @format: The string to print
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+}
